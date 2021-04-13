5ffe3892e24ae539b3d041fae02afc51a6c469ae
Ha Quang Trinh <trinhhqce130226@fpt.edu.vn>
2021-04-13 16:55:16 +0700

    fix authen feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:swp409/Services/Authentication/splash/splash_screen.dart';
+import 'package:swp409/theme.dart';
 
 void main() {
-  runApp(SplashScreen());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clinic booking',
+      theme: theme(),
+      home: SplashScreen(),
+    );
+  }
 }
