f099c17f9a714d3e393b48153c9c991a12a01cfe
KhangLama <lamminhkhang123a@gmail.com>
2021-04-18 14:37:53 +0700

    Update clinicListView.dart

--- lib/Interface/Home/clinicListView.dart
+++ lib/Interface/Home/clinicListView.dart
@@ -2,9 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:swp409/Models/clinic.dart';
-import 'package:swp409/Services/Booking/booking.dart';
 import 'package:swp409/constants.dart';
-import 'clinicdetailView.dart';
 
 class ClinicListView extends StatefulWidget {
   @override
@@ -12,92 +10,65 @@ class ClinicListView extends StatefulWidget {
 }
 
 class _ClinicListViewState extends State<ClinicListView> {
-  Future fetchClinics() async {
+  List<Clinic> _clinics = <Clinic>[];
+
+  Future<List<Clinic>> fetchClinics() async {
     var fetchdata = await rootBundle.loadString('assets/json/clinic.mock.json');
-    if (fetchdata.isEmpty) {
-      return null;
-    } else {
-      var clinics = json.decode(source)
+    var clinics = <Clinic>[];
+    var clinicsjson = json.decode(fetchdata)['Clinic'] as List;
+    for (var clinic in clinicsjson) {
+      clinics.add(Clinic.fromJson(clinic));
     }
+    return clinics;
   }
 
-  // Future<List<Clinic>> _getClinic() async {
-  //   var data = await DefaultAssetBundle.of(context)
-  //       .loadString('assets/json/clinic.mock.json');
-  //   var clinicList = json.decode(data)['Clinic'] as List;
-  //   List<Clinic> clinics = [];
-  //   for (var c in clinicList) {
-  //     Clinic clinic =
-  //         Clinic(id: c['id'], lat: c['lat'], lng: c['lng'], name: c['name']);
-  //     clinics.add(clinic);
-  //   }
-  //   return clinics;
-  // }
+  @override
+  void initState() {
+    fetchClinics().then((value) {
+      setState(() {
+        _clinics.addAll(value);
+      });
+    });
+    super.initState();
+    print(_clinics.length);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.tealAccent,
-      appBar: AppBar(
-        title: Text(
-          'Find Clinic You Want',
-          style: TextStyle(color: kPrimaryLightColor),
-        ),
-        backgroundColor: Colors.lightBlue,
-      ),
-      body: SafeArea(
-          child: Column(
-        children: [
-          Expanded(
-            child: FutureBuilder(
-                future: _getClinic(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.data == null) {
-                    return Container(
-                      child: Center(child: Text('Loading...')),
-                    );
-                  } else
-                    return ListView.builder(
-                      itemCount: snapshot.data.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) => Container(
-                        width: MediaQuery.of(context).size.width,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        child: GestureDetector(
-                          onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ClinicPage())),
-                          child: Card(
-                              child: Row(
-                            children: [
-                              Image(
-                                image: AssetImage('images/0-1.png'),
-                                height: 100,
-                                width: 150,
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(snapshot.data[index].name),
-                                  ElevatedButton(
-                                      onPressed: () => Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => Booking())),
-                                      child: Text('Book an appointment'))
-                                ],
-                              )
-                            ],
-                          )),
-                        ),
-                      ),
-                    );
-                }),
+        backgroundColor: Colors.tealAccent,
+        appBar: AppBar(
+          title: Text(
+            'Find Clinic You Want',
+            style: TextStyle(color: kPrimaryLightColor),
           ),
-        ],
-      )),
-    );
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: _clinics.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                        child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 32, bottom: 32, left: 16, right: 16),
+                      child: Row(
+                        children: [
+                          Image(image: AssetImage('images/0-1.png')),
+                          Column(
+                            children: [
+                              Text(_clinics[index].id),
+                              Text(_clinics[index].name),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ));
+                  }),
+            ),
+          ],
+        ));
   }
 }
