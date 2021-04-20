38c316f27e4142de7cd048697025c37ec30b6217
KhangLama <lamminhkhang123a@gmail.com>
2021-04-20 20:25:56 +0700

    camera

--- lib/Interface/Profile/components/profile_pic.dart
+++ lib/Interface/Profile/components/profile_pic.dart
@@ -1,21 +1,33 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ProfilePic extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfilePic extends StatefulWidget {
   const ProfilePic({
     Key key,
   }) : super(key: key);
 
+  @override
+  _ProfilePicState createState() => _ProfilePicState();
+}
+
+class _ProfilePicState extends State<ProfilePic> {
+  PickedFile _imageFile;
+  final ImagePicker _picker = ImagePicker();
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 220,
       width: 220,
       child: Stack(
-        clipBehavior: Clip.none, fit: StackFit.expand,
+        clipBehavior: Clip.none,
+        fit: StackFit.expand,
         children: [
           CircleAvatar(
-            backgroundImage: AssetImage('images/userprofile.jpg'),
-
+            backgroundImage: _imageFile == null
+                ? AssetImage('images/userprofile.jpg')
+                : FileImage(File(_imageFile.path)),
           ),
           Positioned(
             right: 25,
@@ -30,15 +42,65 @@ class ProfilePic extends StatelessWidget {
                   side: BorderSide(color: Color(0xFFDFDFE3)),
                 ),
                 padding: EdgeInsets.all(10.0),
-                child: Column( // Replace with a Row for horizontal icon + text
+                child: Column(
+                  // Replace with a Row for horizontal icon + text
                   children: <Widget>[
                     Icon(Icons.camera_alt),
                   ],
                 ),
                 color: Color(0xFFDFDFE3),
-                onPressed: () {},
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context, builder: ((builder) => bottomSheet()));
+                },
               ),
             ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void TakePhoto(ImageSource source) async {
+    final pickedFile = await _picker.getImage(source: source);
+    setState(() {
+      _imageFile = pickedFile;
+    });
+  }
+
+  Widget bottomSheet() {
+    return Container(
+      height: 100.0,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Choose Profile Photo',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // ignore: deprecated_member_use
+              FlatButton.icon(
+                  onPressed: () {
+                    TakePhoto(ImageSource.camera);
+                  },
+                  icon: Icon(Icons.camera),
+                  label: Text('Camera')),
+              // ignore: deprecated_member_use
+              FlatButton.icon(
+                  onPressed: () {
+                    TakePhoto(ImageSource.gallery);
+                  },
+                  icon: Icon(Icons.image),
+                  label: Text('Camera')),
+            ],
           )
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_config/flutter_config.dart';
 import 'package:swp409/Services/Authentication/splash/splash_screen.dart';
 import 'package:swp409/theme.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await FlutterConfig.loadEnvVariables();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print(FlutterConfig.get('GOOGLE_MAP_KEY'));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Clinic booking',
